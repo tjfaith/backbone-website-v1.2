606cee0522d4881008dc03f61947e929d063a043
tjfaith <tjbenbiz@gmail.com>
2025-02-07 20:43:42 +0100

    update copies

--- components/AboutUs/AboutHero.tsx
+++ components/AboutUs/AboutHero.tsx
@@ -4,43 +4,42 @@ import Image from "next/image";
 
 import { About1, Bar } from "@/public/assets";
 const AboutHero = () => {
-  return (
-    <div className=" page-max-width">
-      <div className="flex flex-col items-center mb-9">
-        <div className="clash-display-font text-2xl md:text-5xl xl:text-6xl text-center space-x-2 font-medium mb-3 flex-col md:flex-row flex md:items-start">
-          <span className="text-primary" data-aos="fade-up">
-            The Power of
-          </span>
-          <div
-            className="dark:text-secondary-600 text-secondary  flex flex-col"
-            data-aos="fade-up"
-          >
-            <span>
-              Possibilities
-              <Image
-                alt="bar"
-                className=" w-full"
-                data-aos="fade-up"
-                src={Bar}
-              />
-            </span>
-          </div>
+    return (
+        <div className=" page-max-width">
+            <div className="flex flex-col items-center mb-9">
+                <div className="clash-display-font text-2xl md:text-5xl xl:text-6xl text-center space-x-2 font-medium mb-3 flex-col md:flex-row flex md:items-start">
+                    <span className="text-primary" data-aos="fade-up">
+                        The Power of
+                    </span>
+                    <div
+                        className="dark:text-secondary-600 text-secondary  flex flex-col"
+                        data-aos="fade-up"
+                    >
+                        <span>
+                            Possibilities
+                            <Image
+                                alt="bar"
+                                className=" w-full"
+                                data-aos="fade-up"
+                                src={Bar}
+                            />
+                        </span>
+                    </div>
+                </div>
+                <div
+                    className="text-primary-600 text-lg font-normal md:max-w-3xl text-center"
+                    data-aos="fade-up"
+                >
+                    Backbone is a B2B, B2C, and C2C Payments company enabling possibilities for Africans anywhere in the world.
+                </div>
+            </div>
+            <HeroImage
+                alt="about banner"
+                className="w-screen object-cover h-[63vh] object-center "
+                src={About1.src}
+            />
         </div>
-        <div
-          className="text-primary-600 text-lg font-normal md:max-w-3xl text-center"
-          data-aos="fade-up"
-        >
-          Backbone is a payments company empowering Africans worldwide with
-          seamless transactions and endless possibilities.
-        </div>
-      </div>
-      <HeroImage
-        alt="about banner"
-        className="w-screen object-cover h-[63vh] object-center "
-        src={About1.src}
-      />
-    </div>
-  );
+    );
 };
 
 export default AboutHero;
